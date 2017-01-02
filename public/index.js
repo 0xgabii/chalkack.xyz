e32d17751d13652fd4bf3cb1dd5fa6428142aeb0
gomonk3037 <gomonk3037@gmail.com>
2017-01-03 02:27:32 +0900

    v1.0.1
    js css 분리

--- public/index.js
+++ public/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../components/App'
-import 'normalize-scss';
-import '../scss/main.scss';
 
 const rootElement = document.getElementById('app');
 ReactDOM.render(<App />, rootElement);
